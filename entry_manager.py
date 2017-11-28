ed0383f9edb7eb6b5fb8494d40d3a2149d208337
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-11-28 15:00:15 +0300

    Abstract away filename logic

--- entry_manager.py
+++ entry_manager.py
@@ -3,6 +3,20 @@ from interfaces import EntryManager
 import os
 
 
+class Converter(object):
+	def __init__(self):
+		super(Converter, self).__init__()
+
+	def is_entry(self, x):
+		return x.endswith('.tar')
+
+	def from_filename(self, x):
+		return x[:-4]
+
+	def to_filename(self, x):
+		return x+'.tar'
+
+
 class EntryManagerFilesystem(EntryManager):
 	"""
 	EntryManagerFilesystem is an implementation of EntryManager that works
@@ -11,23 +25,24 @@ class EntryManagerFilesystem(EntryManager):
 	def __init__(self, path):
 		super(EntryManagerFilesystem, self).__init__()
 		self.path = path
+		self.conv = Converter()
 
 	def list(self):
 		src = os.listdir(self.path)
 		ret = []
 		for i in src:
-			if i.endswith('.tar'):
-				ret += [i[:-4]]
+			if self.conv.is_entry(i):
+				ret += [self.conv.from_filename(i)]
 		ret.sort()
 		return ret
 
 	def remove(self, entry):
-		filename = os.path.join(self.path, entry+'.tar')
+		filename = os.path.join(self.path, self.conv.to_filename(entry))
 		#print('remove:', filename)
 		os.remove(filename)
 
 	def _add(self, entry):
-		filename = os.path.join(self.path, entry+'.tar')
+		filename = os.path.join(self.path, self.conv.to_filename(entry))
 		#print('_add:', filename)
 		fd = os.open(filename, os.O_CREAT)
 		os.close(fd)
