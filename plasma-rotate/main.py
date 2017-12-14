9d2fac202516511b558bc5b231ead51d0afbc950
m1kc (Max Musatov) <m1kc@yandex.ru>
2017-12-14 16:20:12 +0300

    Teach plasma-rotate to run in any folder

--- plasma-rotate/main.py
+++ plasma-rotate/main.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
+
 from decider import Decider
 from entry_manager import EntryManagerFilesystem
 
 import json
+import sys
 
 
-CONFIG_PATH = "/etc/plasma-rotate.json"
+FILENAME = 'plasma-rotate.json'
 
-testConfig = {
-	'policy': {
-		'daily': 5,
-		'weekly': 3,
-		'monthly': 2,
-	},
-	'path': '/home/m1kc/work/Still-experimental/plasma-rotate/tdtdtdtdtd',
-}
+TEST_PATH = '/home/m1kc/work/Still-experimental/plasma/tdtdtdtdtd'
 
 def main():
+	print("This is the name of the script: ", sys.argv[0])
 	print("Plasma 1.0")
 	print("- written by m1kc")
 	print("- https://github.com/m1kc/plasma")
@@ -26,22 +22,25 @@ def main():
 	print("USE AT YOUR OWN RISK.")
 	print("")
 
-	config = load_config()
-	em = EntryManagerFilesystem(config['path'])
+	path = sys.argv[1]
+
+	print("Rotating entries in:", path)
+
+	config = load_json(path+'/'+FILENAME)
+	em = EntryManagerFilesystem(path)
 	d = Decider(em, config['policy'])
 
-	print("Scanning", config['path'], '...')
 	print("Entries exist:", len(em.list()))
 	d.execute()
 
 	print("Everything's fine, exiting.")
 
 
-def load_config():
-	with open(CONFIG_PATH) as f:
+def load_json(filename):
+	with open(filename) as f:
 		s = f.read()
 		config = json.loads(s)
-		print("Loaded config from", CONFIG_PATH)
+		print("Loaded config from", filename)
 		return config
 
 
